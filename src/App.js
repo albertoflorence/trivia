4d2bb0a87f648040aefc3643becc95d94cf22113
albertoflorence <alberto.sabatier@gmail.com>
2023-06-24 11:27:30 -0300

    feat: requisito 11 - o jogo deve ter apenas 5 perguntas

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Game from './pages/Game';
 import './App.css';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/settings" component={ Settings } />
+      <Route exact path="/feedback" component={ Feedback } />
       <Route path="/game" component={ Game } />
     </Switch>
   );
