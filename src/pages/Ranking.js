73aa0e138ac2feb279cc875a223f741eecd36698
albertoflorence <alberto.sabatier@gmail.com>
2023-06-28 13:42:14 -0300

    feat: style - página de ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
 import Star from '../components/icons/Star';
-import { getRanking } from '../utils';
+import Logo from '../components/Logo';
+import { getRanking, gravatarImage } from '../utils';
 
 class Ranking extends Component {
   render() {
     const { history } = this.props;
     const ranking = getRanking();
-    console.log(ranking);
     ranking.sort((a, b) => b.score - a.score);
-    console.log(ranking);
 
     return (
-      <div>
-        <h2 data-testid="ranking-title">Ranking</h2>
-        {ranking.map(({ score, name, gravatarEmail }, index) => (
-          <div key={ index }>
-            <img src={ gravatarEmail } alt="imagem perfil" />
-            <span data-testid={ `player-name-${index}` }>{name}</span>
-            <div>
-              <Star />
-              <span data-testid={ `player-score-${index}` }>{score}</span>
+      <div className="ranking">
+        <div className="ranking-content">
+          <Logo />
+          <h2 data-testid="ranking-title">Ranking</h2>
+          {ranking.map(({ score, name, gravatarEmail }, index) => (
+            <div key={ index } className="ranking-item">
+              <img src={ gravatarImage(gravatarEmail) } alt="imagem perfil" />
+              <span data-testid={ `player-name-${index}` }>{name}</span>
+              <div className="ranking-score">
+                <Star />
+                <strong>
+                  <span data-testid={ `player-score-${index}` }>{score}</span>
+                </strong>
+                pontos
+              </div>
             </div>
-          </div>
-
-        ))}
-        <button
-          data-testid="btn-go-home"
-          onClick={ () => history.push('/') }
-        >
-          Jogar novamente
-        </button>
+          ))}
+          <button
+            data-testid="btn-go-home"
+            className="btn-again"
+            onClick={ () => history.push('/') }
+          >
+            Jogar novamente
+          </button>
+        </div>
       </div>
     );
   }
